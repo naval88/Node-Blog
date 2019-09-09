7c160d21399d067e4be3c940c6c5f8252fd1229d
naval <navalkishor2005@gmail.com>
2019-09-09 12:53:48 +0530

    test

--- config/db.js
+++ config/db.js
@@ -1,6 +1,4 @@
 var mysql = require('mysql');
-var env = require('./env.js');
-console.log(env.db_host);
 //local mysql db connection
 var connection = mysql.createConnection({
     host     :  'us-cdbr-iron-east-02.cleardb.net',
