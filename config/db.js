4c8b9b666a3c67594d547eb3b33cc119082f2216
naval <navalkishor2005@gmail.com>
2019-09-10 18:59:21 +0530

    update db file

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
 var mysql = require('mysql');
-var env = require('./env.js');
-console.log(env.db_host);
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     :  env.db_host,
-    user     :  env.db_user_name,
-    password :  env.db_password,
-    database :  env.db_name
+    host     :  'us-cdbr-iron-east-02.cleardb.net',
+    user     :  'b57b2ab00fb679',
+    password :  '75f10a8d',
+    database :  'heroku_5108a8ced7246ad'
 });
 
 connection.connect(function(err) {
